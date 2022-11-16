18be01c0d8599aa56d123936409a7bc507cb3475
Yelena Cox <yelenacox@gmail.com>
2022-11-16 14:02:52 -0600

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.Write("Who would you like to say hello to? ");
+
+string name = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("Fine, don't say 'hello'!");
+}
+else 
+{
+    Console.WriteLine($"Hello, {name}!");
+}
